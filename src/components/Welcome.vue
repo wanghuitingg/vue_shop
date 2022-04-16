3100587f97e09813ea6df71c9f330831905dee68
wanghuitingg <1687752378@qq.com>
2022-04-16 18:23:46 +0800

    add

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
 	<!-- 用户首页 欢迎组件 -->
 	<div align="center">
-		<img src="../assets/rice.png" alt="">
+		<img src="../assets/rice.jpg" alt="">
 		<div style="font-size: 20px;">饭否 欢迎你</div>
 	</div>
 </template>
